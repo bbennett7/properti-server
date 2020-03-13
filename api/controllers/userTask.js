04501256575ed1d2123b7a9e58509031b79be784
bbennett7 <bryn.bennett@gmail.com>
2020-03-13 15:45:39 -0700

    Update user task by id

--- api/controllers/userTask.js
+++ api/controllers/userTask.js
@@ -1,55 +1,76 @@
 const Joi = require('joi');
 const uniqid = require('uniqid');
-const userTask = require('../models/userTask');
+const moment = require('moment');
+const userTaskModel = require('../models/userTask');
 
-const validateCreateBody = body => {}
+const validateCreateBody = body => {};
 
 const createUserTask = async (req, res) => {
-    // add in task validations
-    const userTaskId = uniqid();
-    const { id } = req.params;
-    try {
-        const data = await userTask.createUserTask(id, userTaskId, req.body);
-        if (data.length > 0) {
-            return res.status(200).send(data[0]);
-        }
-
-        return res.status(400).send('Error occurred creating task for user.');
-    } catch (err) {
-        return res.status(500).send(err);
+  // add in task validations
+  const userTaskId = uniqid();
+  const { id } = req.params;
+  try {
+    const data = await userTaskModel.createUserTask(id, userTaskId, req.body);
+    if (data.length > 0) {
+      return res.status(200).send(data[0]);
     }
-}
+
+    return res.status(400).send('Error occurred creating task for user.');
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
 
 const getTasksByUserId = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const data = await userTask.getTasksByUserId(id);
+  const { id } = req.params;
+  try {
+    const data = await userTaskModel.getTasksByUserId(id);
 
-        return res.status(200).send(data);
-    } catch (err) {
-        return res.status(500).send(err);
+    return res.status(200).send(data);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
+
+const updateUserTaskById = async (req, res) => {
+  const { userId, id } = req.params;
+  const { body } = req;
+
+  if (body.status === 'Completed') {
+    body.completed_on = moment();
+  }
+
+  try {
+    const data = await userTaskModel.updateUserTaskById(userId, id, body);
+
+    if (data.length > 0) {
+      return res.status(200).send(data[0]);
     }
-}
 
-const updateUserTaskById = async (req, res) => {}
+    return res.status(400).send('Error updating task for user.');
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
+};
 
 const deleteUserTaskById = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const data = await userTask.deleteUserTaskById(id);
-        if (data.length > 0) {
-            return res.status(200).send(data[0]);
-        }
-
-        res.status(400).send('Error deleting task for user.')
-    } catch (err) {
-        return res.status(500).send(err);
+  const { userId, id } = req.params;
+  try {
+    const data = await userTaskModel.deleteUserTaskById(userId, id);
+    if (data.length > 0) {
+      return res.status(200).send(data[0]);
     }
-}
+
+    return res.status(400).send('Error deleting task for user.');
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
 
 module.exports = {
-    createUserTask,
-    getTasksByUserId,
-    updateUserTaskById,
-    deleteUserTaskById
-}
+  createUserTask,
+  getTasksByUserId,
+  updateUserTaskById,
+  deleteUserTaskById
+};
--- api/models/userTask.js
+++ api/models/userTask.js
@@ -1,21 +1,21 @@
+const moment = require('moment');
 const pool = require('../../config/pg-config');
-const moment = require('moment')
 
 const createUserTask = async (id, userTaskId, body) => {
-    const createdOn = moment()
-    const values = [
-        userTaskId,
-        id,
-        body.task_id,
-        body.urgency_level,
-        body.status,
-        body.notes,
-        createdOn,
-        null
-    ] 
+  const createdOn = moment();
+  const values = [
+    userTaskId,
+    id,
+    body.task_id,
+    body.urgency_level,
+    body.status,
+    body.notes,
+    createdOn,
+    null
+  ];
 
-    return pool.query(
-        `INSERT INTO users_tasks(
+  return pool.query(
+    `INSERT INTO users_tasks(
             id,
             user_id,
             task_id,
@@ -27,31 +27,62 @@ const createUserTask = async (id, userTaskId, body) => {
         ) VALUES (
             $1, $2, $3, $4, $5, $6, $7, $8
         ) RETURNING *`,
-        values
-    )
-}
+    values
+  );
+};
 
-const getTasksByUserId = async (id) => {
-    return pool.query(
-        `SELECT *
+const getTasksByUserId = async id => {
+  return pool.query(
+    `SELECT *
         FROM users_tasks
         WHERE user_id IN ($1)`,
-        [id]
-    )
-}
+    [id]
+  );
+};
 
-const deleteUserTaskById = async id => {
-    return pool.query(
-        `DELETE
+const updateUserTaskById = async (userId, id, body) => {
+  const values = [];
+  const keys = [];
+
+  values.push(id);
+  values.push(userId);
+  Object.keys(body).forEach(key => {
+    if (!body[key] || key === 'id' || key === 'user_id' || key === 'task_id') {
+      return null;
+    }
+
+    if (key !== id) {
+      values.push(body[key]);
+      keys.push(key);
+    }
+  });
+
+  let query = '';
+  let count = 2;
+  keys.forEach((key, i) => {
+    count += 1;
+    query += `${key}=($${count})${keys.length - 1 !== i ? ', ' : ''}`;
+  });
+
+  return pool.query(
+    `UPDATE users_tasks SET ${query} WHERE id IN($1) AND user_id IN($2) RETURNING *`,
+    values
+  );
+};
+
+const deleteUserTaskById = async (userId, id) => {
+  return pool.query(
+    `DELETE
         FROM users_tasks
-        WHERE id IN ($1)
+        WHERE id IN ($1) AND user_id IN ($2)
         RETURNING *`,
-        [id]
-    )
-}
+    [id, userId]
+  );
+};
 
 module.exports = {
-    createUserTask,
-    getTasksByUserId,
-    deleteUserTaskById
-}
+  createUserTask,
+  getTasksByUserId,
+  updateUserTaskById,
+  deleteUserTaskById
+};
