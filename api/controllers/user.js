94c921e9114cfbe14d88e98af0e6050bd171b2ab
bbennett7 <bryn.bennett@gmail.com>
2020-03-13 15:45:20 -0700

    Update user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,52 +1,68 @@
-const Joi = require('joi');
+//const Joi = require('joi');
 const userModel = require('../models/user');
 
-const validateCreateBody = body => {}
+//const validateCreateBody = body => {};
 
 const createUser = async (req, res) => {
-    // add in user validations
-    try {
-        const data = await userModel.createUser(req.body);
-        if (data.length > 0) {
-            return res.status(200).send(data[0]);
-        }
-
-        throw Error('Error occurred creating user.');
-    } catch (err) {
-        return res.status(500).send(err);
+  // add in user validations
+  try {
+    const data = await userModel.createUser(req.body);
+    if (data.length > 0) {
+      return res.status(200).send(data[0]);
     }
-}
+
+    return res.status(400).send('Error occurred creating user.');
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
 
 const getUserById = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const data = await userModel.getUserById(id);
-        if (data.length > 0) {
-            return res.status(200).send(data[0]);
-        }
-
-        res.status(400).send('User not found.')
-    } catch (err) {
-        return res.status(500).send(err);
+  const { id } = req.params;
+  try {
+    const data = await userModel.getUserById(id);
+    if (data.length > 0) {
+      return res.status(200).send(data[0]);
     }
-}
+
+    return res.status(400).send('User not found.');
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
+
+const updateUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const data = await userModel.updateUserById(id, req.body);
+    if (data.length > 0) {
+      return res.status(200).send(data[0]);
+    }
+
+    return res.status(400).send('Error updating user.');
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
 
 const deleteUserById = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const data = await userModel.deleteUserById(id);
-        if (data.length > 0) {
-            return res.status(200).send(data[0]);
-        }
-
-        res.status(400).send('Error deleting user.')
-    } catch (err) {
-        return res.status(500).send(err);
+  const { id } = req.params;
+  try {
+    const data = await userModel.deleteUserById(id);
+    if (data.length > 0) {
+      return res.status(200).send(data[0]);
     }
-}
+
+    return res.status(400).send('Error deleting user.');
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
 
 module.exports = {
-    createUser,
-    getUserById,
-    deleteUserById
-}
+  createUser,
+  getUserById,
+  updateUserById,
+  deleteUserById
+};
--- api/models/user.js
+++ api/models/user.js
@@ -1,17 +1,17 @@
 const pool = require('../../config/pg-config');
 
 const createUser = async body => {
-    const values = [
-        body.id,
-        body.email,
-        body.first_name,
-        body.last_name,
-        body.unit,
-        body.account_type
-    ]
+  const values = [
+    body.id,
+    body.email,
+    body.first_name,
+    body.last_name,
+    body.unit,
+    body.account_type
+  ];
 
-    return pool.query(
-        `INSERT INTO users(
+  return pool.query(
+    `INSERT INTO users(
             id,
             email,
             first_name,
@@ -21,31 +21,58 @@ const createUser = async body => {
         ) VALUES (
             $1, $2, $3, $4, $5, $6
         ) RETURNING *`,
-        values
-    )
-}
+    values
+  );
+};
 
 const getUserById = async id => {
-    return pool.query(
-        `SELECT *
+  return pool.query(
+    `SELECT *
         FROM users
         WHERE users.id = ($1)`,
-        [id]
-    )
-}
+    [id]
+  );
+};
+
+const updateUserById = async (id, body) => {
+  const values = [];
+  const keys = [];
+
+  values.push(id);
+  Object.keys(body).forEach(key => {
+    if (!body[key] || key === 'id') {
+      return null;
+    }
+
+    if (key !== id) {
+      values.push(body[key]);
+      keys.push(key);
+    }
+  });
+
+  let query = '';
+  let count = 1;
+  keys.forEach((key, i) => {
+    count += 1;
+    query += `${key}=($${count})${keys.length - 1 !== i ? ', ' : ''}`;
+  });
+
+  return pool.query(`UPDATE users SET ${query} WHERE id IN($1) RETURNING *`, values);
+};
 
 const deleteUserById = async id => {
-    return pool.query(
-        `DELETE
+  return pool.query(
+    `DELETE
         FROM users
         WHERE id IN ($1)
         RETURNING *`,
-        [id]
-    )
-}
+    [id]
+  );
+};
 
 module.exports = {
-    createUser,
-    getUserById,
-    deleteUserById
-}
+  createUser,
+  getUserById,
+  deleteUserById,
+  updateUserById
+};
